1933660138bfe85ea9a63ef29ea9a7b4f5b11b34
EZaplata <raypol97@gmail.com>
2014-04-01 12:21:26 +0100

    Create action.c

---
+++ action.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+void action(int option){
+int setup;
+if(option == 1){
+game( setup );
+}
+else if(option == 2){
+    printf("Work in progress(The alpha version is set at Normal difficulty level)\n");
+    printf("What difficulty level you want to play:\n1.Easy\n2.Normal\n3.Hard\n4.HELL!!\n\n");
+    main();
+}
+else{
+
+}
+}

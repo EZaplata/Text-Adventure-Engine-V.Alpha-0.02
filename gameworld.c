c52057ae3a197fc879a3f716904f0658b495d4d1
EZaplata <raypol97@gmail.com>
2014-04-06 20:42:18 +0100

    Update gameworld.c

--- gameworld.c
+++ gameworld.c
@@ -2,10 +2,13 @@
 
 void gameworld(){
 int test;
-printf("Test fight:Type in 1 to fight");
+printf("Test fight:Type in 1 to fight\nItem generator:Type in 2 to generate\n");
 scanf("%d", &test);
 if(test == 1){
     intelligence();
+}
+else if(test == 2){
+    itemgenerator();
 }
 
 

7889f39d9649a1861d28bc7da619713fa6e02c97
EZaplata <raypol97@gmail.com>
2014-04-09 16:05:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ printf("\t1.Start\n");
 printf("\t2.Options\n");
 printf("\t3.Multiplayer\n");
 printf("\t4.Exit\n");
-printf("\n\n\t\t\t\t\t\t\t Version:Alpha 0.03\n");
+printf("\n\n\t\t\t\t\t\t\t Version:Alpha 0.031\n");
 printf("Developed by Eryk Zaplata\n");
 scanf("%d", &option);
 action ( option );

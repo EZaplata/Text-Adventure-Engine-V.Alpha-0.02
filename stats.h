ec0c10823c063c66a511d3896e2ed025c55fbc3a
EZaplata <raypol97@gmail.com>
2014-04-26 18:47:43 +0100

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,1 +1,19 @@
-extern int
+//Player
+extern int str;
+extern int agi;
+extern int def;
+extern int hp;
+extern int mp;
+extern char pfname[20];
+extern char psname[20];
+//Monster
+extern int mstr;
+extern int magi;
+extern int mdef;
+extern int mhp;
+extern int mmp;
+
+extern int exp;
+extern int lvl;
+extern int plvl;
+extern int rexp;

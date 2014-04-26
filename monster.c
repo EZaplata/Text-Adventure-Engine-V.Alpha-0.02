97fdd7ede3a3329f8e8b0461d3e90cce442ff2a5
EZaplata <raypol97@gmail.com>
2014-04-26 18:47:11 +0100

    Update monster.c

--- monster.c
+++ monster.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 
+int mstr = 4;
+int magi = 5;
+int mdef = 5;
+int mhp = 30;
+int mmp = 8;
+
 int monster(){
+    return mstr++;
+    return magi++;
+    return mhp++;
+    return mmp++;
+
 }

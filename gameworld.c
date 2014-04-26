52165f0696c9476eae25b5d09bbb2d11b570b352
EZaplata <raypol97@gmail.com>
2014-04-26 18:45:46 +0100

    Update gameworld.c

--- gameworld.c
+++ gameworld.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include "stats.h"
+
 
 void gameworld(){
 int test;
-printf("Test fight:Type in 1 to fight\nItem generator:Type in 2 to generate\n");
+printf("Test fight:Type in 1 to fight\nItem generator:Type in 2 to generate\nCheck stats 3\n");
 scanf("%d", &test);
 if(test == 1){
     intelligence();
@@ -10,6 +12,14 @@ if(test == 1){
 else if(test == 2){
     itemgenerator();
 }
+else if(test == 3){
+    printf("Level:\n%d\nStats:\n strength=%d \n agility=%d \n defense=%d \n health=%d \n magic=%d \n\n",lvl, str ,agi ,def ,hp, mp);
+    printf("Current experience:%d\nNeeded to level up:%d\n\n", exp, rexp);
+    gameworld();
+}
+if(lvl > plvl){
+        player();
+    }
 
 
 }

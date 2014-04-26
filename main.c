543d879d3692dc959148fb2102365d69fd04ca48
EZaplata <raypol97@gmail.com>
2014-04-26 18:46:56 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,11 +13,34 @@ void menu(){
 printf("\t1.Start\n");
 printf("\t2.Options\n");
 printf("\t3.Multiplayer\n");
-printf("\t4.Exit\n");
-printf("\n\n\t\t\t\t\t\t\t Version:Alpha 0.031\n");
-printf("Developed by Eryk Zaplata\n");
+printf("\t4.Credits\n");
+printf("\t5.Exit\n");
+printf("\n\n\t\t\t\t\t\t\t Version:Alpha 0.04\n");
 scanf("%d", &option);
 action ( option );
 
 
+}
+void action(int option){
+int setup;
+if(option == 1){
+game( setup );
+}
+else if(option == 2){
+    printf("Work in progress(The alpha version is set at Normal difficulty level)\n");
+    printf("What difficulty level you want to play:\n1.Easy\n2.Normal\n3.Hard\n4.HELL!!\n\n");
+    main();
+}
+else if (option == 3){
+        printf("Work in progress\n");
+        main();
+}
+else if (option == 4){
+    printf("Developed by:\n\tEryk Zaplata\n\n");
+    main();
+}
+
+else{
+
+}
 }
